d2fc1785599b5f66742af0ad85d74f3f8f674d37
AUryeong <jykee7030@gmail.coma>
2023-11-14 09:33:47 +0900

    Add Stage 3 Song and UI Sprites

--- Assets/A/Scripts/InGame/Enemy.cs
+++ Assets/A/Scripts/InGame/Enemy.cs
@@ -31,7 +31,7 @@ namespace InGame
             gameObject.layer = LayerMask.NameToLayer("Enemy");
 
             transform.localPosition = defaultLocalPos;
-            
+
             bone.localPosition = Vector3.zero;
             bone.rotation = defaultBoneQuaternion;
             bone.DOLocalMove(spawnVector, TileManager.Instance.beatInterval * 2).From(true);
@@ -76,30 +76,33 @@ namespace InGame
             gameObject.layer = LayerMask.NameToLayer("DeathEnemy");
             SoundManager.Instance.PlaySound("clap", ESoundType.Sfx, 0.5f);
 
-            bone.DOMoveX(Random.Range(0,2) == 0 ? Random.Range(-15f,-20f) : Random.Range(15f,20f), ENEMY_MOVE_DURATION).SetRelative(true);
-            bone.DOMoveY(Random.Range(5f, 10f), ENEMY_MOVE_DURATION).SetEase(Ease.OutBack).SetRelative(true);
-            bone.DOMoveZ(Random.Range(30, 50f), ENEMY_MOVE_DURATION).SetRelative(true).OnComplete(() => gameObject.SetActive(false));
-            bone.DORotateQuaternion(Quaternion.Euler(Random.Range(-360f, 360f), Random.Range(-360f, 360f), Random.Range(-360f, 360f)), ENEMY_MOVE_DURATION);
+            // bone.DOMoveX(Random.Range(0,2) == 0 ? Random.Range(-15f,-20f) : Random.Range(15f,20f), ENEMY_MOVE_DURATION).SetRelative(true);
+            // bone.DOMoveY(Random.Range(5f, 10f), ENEMY_MOVE_DURATION).SetEase(Ease.OutBack).SetRelative(true);
+            // bone.DOMoveZ(Random.Range(30, 50f), ENEMY_MOVE_DURATION).SetRelative(true).OnComplete(() => gameObject.SetActive(false));
+            // bone.DORotateQuaternion(Quaternion.Euler(Random.Range(-360f, 360f), Random.Range(-360f, 360f), Random.Range(-360f, 360f)), ENEMY_MOVE_DURATION);
+            gameObject.SetActive(false);
 
             Player.Instance.hitAbleEnemyList.Remove(this);
             Player.Instance.Hp += ENEMY_HP_HEAL_VALUE;
 
             float distance = transform.position.z - Player.Instance.transform.position.z;
-            if (distance <= 2.5f)
+            if (distance <= 4f)
             {
                 InGameManager.Instance.Rune += 9;
+                PoolManager.Instance.Init("Perfect Effect").transform.position = transform.position;
             }
-            else if (distance <= 4)
+            else if (distance <= 5)
             {
                 InGameManager.Instance.Rune += 7;
+                PoolManager.Instance.Init("Great Effect").transform.position = transform.position;
             }
             else
             {
                 InGameManager.Instance.Rune += 5;
+                PoolManager.Instance.Init("Good Effect").transform.position = transform.position;
             }
 
-            var obj = PoolManager.Instance.Init("Hit Effect");
-            obj.transform.position = transform.position;
+            PoolManager.Instance.Init("Hit Effect").transform.position = transform.position;
         }
     }
 }
